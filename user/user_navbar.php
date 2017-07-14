132b012e03099f90ef7a991a3addfda593ca93de
zeel91297 <zeel91297@gmail.com>
2017-07-14 19:45:26 +0530

    css js userheader login

---
+++ user/user_navbar.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../shared/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../shared/css/justified-nav.css" rel="stylesheet">
+    <link href="../shared/css/signin.css" rel="stylesheet">
+
+    <script src="../shared/js/jquery-3.2.1.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="../shared/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../shared/bootstrap-theme.min.css">
+    <!-- jQuery library -->
+    <!-- Latest compiled JavaScript -->
+    <script src="../shared/css/bootstrap.min.js"></script>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="../shared/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <div class="masthead">
+        <h3 class="text-muted">
+            <img src="../logo.png">
+        </h3>
+
+        <nav class="navbar navbar-light bg-faded rounded mb-3">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-toggleable-md" id="navbarCollapse">
+            <ul class="nav navbar-nav text-md-center justify-content-md-between">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ask a Question</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Question</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                  <a class="dropdown-item" href="#">Question Category</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+              </li>
+
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <!-- Jumbotron -->
+      <!--<div class="jumbotron">
+        <h1>Welcome To InstaAnswer!</h1>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+        -->
+      </div>
+  </body>
+</html>
